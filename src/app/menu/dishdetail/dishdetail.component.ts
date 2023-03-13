343684fd1759ef89e10a439d6ba9cedd3b5d58df
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2023-03-13 15:15:16 +0500

    some changes

--- src/app/menu/dishdetail/dishdetail.component.ts
+++ src/app/menu/dishdetail/dishdetail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from 'src/app/shared/dish';
-import { MenuComponent } from '../menu.component';
 
 @Component({
   selector: 'app-dishdetail',
@@ -11,7 +10,7 @@ export class DishdetailComponent implements OnInit {
 
   @Input() dish!: Dish;
 
-  constructor(public menuComp: MenuComponent) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
