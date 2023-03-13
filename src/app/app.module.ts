e196cc4f31e2be300bc2880ad4cf9f474592b1c0
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2023-03-13 18:35:33 +0500

    Appending header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,18 @@ import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './menu/dishdetail/dishdetail.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
