27d87a9c364dc38c201dc705ea5d12d3bfeeaef1
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-21 18:58:26 +0500

    Angular Custom Attribute Directive - Highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
